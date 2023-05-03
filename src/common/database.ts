9e8d3542936ebc8264b35732b05236a42f36a1ae
James Chan <jameschan312.cn@gmail.com>
2023-05-03 00:33:43 -0400

    feat: *

--- src/common/database.ts
+++ src/common/database.ts
@@ -2,6 +2,12 @@ import { DataSource, InsertResult } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import 'dotenv/config';
 import { Environment } from './enum';
+import UserEntity from '../entity/user.entity';
+import UserProcessEntity from '../entity/user-process.entity';
+import TaskCommentEntity from '../entity/task-comment.entity';
+import SubjectEntity from '../entity/subject.entity';
+import CategoryEntity from '../entity/category.entity';
+import TaskEntity from '../entity/task.entity';
 
 export const DB = new DataSource({
     type: 'mysql',
@@ -11,7 +17,14 @@ export const DB = new DataSource({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DATABASE,
     namingStrategy: new SnakeNamingStrategy(),
-    entities: [__dirname + '/../**/*.entity.js'],
+    entities: [
+        UserEntity,
+        UserProcessEntity,
+        TaskCommentEntity,
+        SubjectEntity,
+        CategoryEntity,
+        TaskEntity,
+    ],
     logging: parseInt(process.env.ENVIRONMENT) in [Environment.DEVELOPMENT, Environment.TEST],
     logger: 'file',
     maxQueryExecutionTime: 1000,
